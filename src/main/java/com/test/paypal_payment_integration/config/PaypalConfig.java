942bf8a9410d8fecc3f2e8c2079ca665dc8525b5
AbetiouSN <sofianeabetiou@gmail.com>
2024-08-20 01:12:27 +0100

    PaypalService & PaypalController & views implementation

--- src/main/java/com/test/paypal_payment_integration/config/PaypalConfig.java
+++ src/main/java/com/test/paypal_payment_integration/config/PaypalConfig.java
@@ -7,12 +7,21 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class PaypalConfig {
-    @Value("${{paypal.client-id}}")
-    private  String clientId;
-    @Value("${{paypal.client-secret}}")
-    private  String clientSecret;
-    @Value("${{paypal.mode}}")
-    private String mode;
+
+    private String clientId = "your clientId";
+    private String clientSecret = "your clientsecret";
+    private String mode = "sandbox"; // just pour le test si l'application reel on utilise live mode
+
+// Ou ( check application.yml
+////    @Value("${paypal.client-id}")
+//    private String clientId;
+//
+//    @Value("${paypal.client-secret}")
+//    private String clientSecret;
+//
+//    @Value("${paypal.mode}")
+//    private String mode;
+
 
     @Bean
     public APIContext apiContext(){
